b213d058626f48a99a633e88b17ee36b87763a0b
Sebastien andre <simac@Sebastiens-iMac.local>
2022-05-03 22:22:43 -0400

    Update

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,10 +2,16 @@
 
 const btnBurger = document.querySelector('.c-btn__burger');
 
+function showMenu(){
+    const menuBg = document.querySelector('.menu-bg');
+    const menu = document.querySelector('.menu');
+}
+
 function btn(e){
     e.preventDefault();
     const btnBurgerIcons = document.querySelector('.c-btn__burger-icons');
     btnBurgerIcons.classList.toggle("toggled");
     console.log('testing');
 }
-btnBurger.addEventListener('click',btn )
+
+btnBurger.addEventListener('click',btn );
